97a474a95bc862ecaacc984c8e8c8d83de313bcb
Drewbi <drewalexander986@gmail.com>
2021-09-09 17:37:58 +0800

    add next home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useMemo } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
+import { BoxBufferGeometry } from 'three'
+import dynamic from 'next/dynamic';
+
+
+const OrbitControls = dynamic(async () => await (await import('drei')).OrbitControls, {
+  ssr: false
+});
 
 const Box = (props) => {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
-
+  const geom = useMemo(() => new BoxBufferGeometry(100, 100, 100))
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
@@ -15,26 +22,25 @@ const Box = (props) => {
   })
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      position={[0, 0, -200]}
-      scale={[200, 200, 200]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'#3090a0'} />
-    </mesh>
+    <group ref={mesh} {...props} scale={[1, 1, 1]} position={[0, 0, 0]}>
+      <lineSegments>
+        <edgesGeometry attach="geometry" args={[geom]} />
+        <lineBasicMaterial color="#63B4D1" attach="material" />
+      </lineSegments>
+      <mesh>
+        <boxBufferGeometry args={[100, 100, 100]} />
+        <meshStandardMaterial color="#222" />
+      </mesh>
+    </group>
   )
 }
 
 export default function Home() {
   return (
-    <Canvas orthographic={true}>
+    <Canvas orthographic={true} camera={{ position: [0, 0, -100] }}>
       <ambientLight />
-      <pointLight position={[1, 1, 200]} />
-      <Box />
+      <Box position={[0, 0, 0]} />
+      <OrbitControls />
     </Canvas>
   )
 }
