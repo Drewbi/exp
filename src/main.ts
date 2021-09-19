ca8d62d63124803c30301bee2d978cb2017315b5
Drewbi <drewalexander986@gmail.com>
2021-09-20 00:04:03 +0800

    remove console log

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,4 @@ import exp from './exp'
 const app = document.querySelector<HTMLDivElement>('#app')!
 const base = '/exp/'
 const path = window.location.pathname
-console.log(path)
 app.innerHTML = path === base ? 'Home' : exp.find(obj => base + obj.path === path)?.src.html ?? '404'
