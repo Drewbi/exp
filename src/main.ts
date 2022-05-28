e7c664199fcdf866414bba4fdb979adbfbb96b36
Drewbi <drewalexander986@gmail.com>
2022-05-28 14:02:58 +0800

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import './style.css'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>E X P</h1>
+  <a href="exp/_/boi">Boi(d)</a>
 `
