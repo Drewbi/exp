a6bf12405b4b207f69c27d0e2f8227eb219f18e5
Drewbi <drewalexander986@gmail.com>
2022-05-28 13:58:29 +0800

    add vite

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/exp/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        boi: resolve(__dirname, '_/boi.html')
+      }
+    }
+  }
+})
