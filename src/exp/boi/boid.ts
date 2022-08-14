ec620f1a588d340f3af8b41676525a5f0f68193e
Drewbi <drewalexander986@gmail.com>
2022-08-14 17:56:20 +0800

    add centre calculation

--- src/exp/boi/boid.ts
+++ src/exp/boi/boid.ts
@@ -1,10 +1,12 @@
-import { CylinderGeometry, MeshBasicMaterial, Mesh, Vector3, MathUtils } from 'three'
-import { Boundary, BOUNDS, BOUNDS_MARGIN } from '../../utils/map'
+import { CylinderGeometry, MeshBasicMaterial, Mesh, Vector3, SphereGeometry } from 'three'
+import { boids, scene } from './main'
+import { calcTurnFactor, getVisibleBoids } from './utils'
 export class Boid {
     geometry: CylinderGeometry
     material: MeshBasicMaterial
     mesh: Mesh
     speed: number
+    cenDot: Mesh
 
     constructor(initialX: number, initialY: number, initialRotation: number) {
         this.geometry = new CylinderGeometry(0, 10, 20, 3, 1)
@@ -13,63 +15,36 @@ export class Boid {
         this.mesh.position.x = initialX
         this.mesh.position.y = initialY
         this.mesh.rotation.z = initialRotation
-        this.speed = 5
+        this.speed = 3
+        const dotGeo = new SphereGeometry(2)
+        const dotMat = new MeshBasicMaterial(
+            { color: '#0099ff' }
+        )
+        const dot = new Mesh(dotGeo, dotMat)
+        this.cenDot = dot
+        scene.add(dot)
     }
 
     updateBoid() {
+        this.flyTowardsCentre()
         this.avoidWalls()
         const forward = new Vector3(0, this.speed, 0).applyAxisAngle(
             new Vector3(0, 0, 1), this.mesh.rotation.z)
         this.mesh.position.add(forward)
     }
 
-    getClosestBoids() {
-        return
+    flyTowardsCentre() {
+        const visiBoids = getVisibleBoids(this, boids, 300)
+        const cen = visiBoids.reduce((prev: Vector3, curr: Boid) => {
+            return prev.clone().add(curr.mesh.position)
+        }, new Vector3(0, 0, 0)).divideScalar(visiBoids.length)
+        this.cenDot.position.set(cen.x, cen.y, cen.z)
+        this.mesh.rotation
     }
 
     avoidWalls() {
-        const turnFactor = this.calcTurnFactor()
+        const turnFactor = calcTurnFactor(this.mesh.position, this.mesh.rotation)
         this.mesh.rotation.z += turnFactor
-    }
-
-    calcTurnFactor() {
-        return BOUNDS.reduce((prev, curr) => {
-            const distanceToEdge = Math.abs(curr.offset - this.mesh.position[curr.axis])
-            if (distanceToEdge < BOUNDS_MARGIN) {
-
-
-                const forward = new Vector3(0, 1, 0).applyAxisAngle(
-                    new Vector3(0, 0, 1), this.mesh.rotation.z)
-
-                const normalisedAngle = forward.angleTo(curr.normal)
-                // console.log('----')
-                // console.log((normalisedAngle / (2 * Math.PI)) * 360)
-                const turnToNormal = normalisedAngle 
-                // console.log((turnToNormal / (2 * Math.PI)) * 360)
-
-                // console.log('----')
-                const distanceFactor = 1 - MathUtils.smoothstep(distanceToEdge, 0, BOUNDS_MARGIN)
-                // console.log(this.calcTurnDirection(curr))
-                return this.calcTurnDirection(curr) * MathUtils.lerp(prev, turnToNormal, distanceFactor) + prev
-            }
-            return prev
-        }, 0)
-    }
-
-    calcTurnDirection(bound: Boundary): 1 | -1 {
-        if (bound.axis === 'x') {
-            if (this.mesh.rotation.z < Math.PI / 2 || this.mesh.rotation.z > (Math.PI / 2) * 3) {
-                return bound.normal[bound.axis] < 0 ? 1 : -1
-            }
-            return bound.normal[bound.axis] < 0 ? -1 : 1
-        }
-        if (bound.axis === 'y') {
-            if (this.mesh.rotation.z < Math.PI) {
-                return bound.normal[bound.axis] < 0 ? 1 : -1
-            }
-            return bound.normal[bound.axis] < 0 ? -1 : 1
-        }
-        return 1
     }
 }
 
