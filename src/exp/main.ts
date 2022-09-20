b5430843a4448a4574ca228bb3ba9aede8687b90
Drew Alexander <drewalexander986@gmail.com>
2022-09-20 17:45:51 +0800

    add base url to menu

--- src/exp/main.ts
+++ src/exp/main.ts
@@ -7,7 +7,7 @@ interface PageConfig {
 
 const configs = import.meta.glob('../**/config.json', { eager: true })
 const modules = Object.values(configs) as PageConfig[]
-const links = modules.map((page): string => `<a href="${page.link}">${page.title}</a>`)
+const links = modules.map((page): string => `<a href="${process.env.base_url}${page.link}">${page.title}</a>`)
 const app = document.querySelector<HTMLDivElement>('#app')
 if (app) {
     app.innerHTML = `<h1>E X P</h1>\n`
