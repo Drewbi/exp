fb610a1c76222bc31dddadcf1d2ca9eb5616876c
Drewbi <drewalexander986@gmail.com>
2024-06-07 02:27:48 -0700

    upgrade new test script

--- scripts/newtest.js
+++ scripts/newtest.js
@@ -7,5 +7,10 @@ var dir = './src/test/' + id;
 if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 
-    fs.cpSync('./src/base/shader', dir, { recursive: true });
+    const args = process.argv.slice(2);
+    const type = args[0] || "shader";
+
+    if (!["flat", "shader"].includes(type)) throw Error(`${type} is not a valid type`)
+    fs.cpSync('./src/base/' + type, dir, { recursive: true });
+    console.log(`Created new ${type} test with id ${id}`)
 }
