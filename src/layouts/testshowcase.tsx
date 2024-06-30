11a05c67bdb1570e970505080743d8a6a74c7249
Drewbi <drewalexander986@gmail.com>
2024-06-30 15:26:44 +0800

    anotha one

--- src/layouts/testshowcase.tsx
+++ src/layouts/testshowcase.tsx
@@ -5,7 +5,7 @@ export default function () {
     return (
         <div className='w-full h-full p-10 grid grid-cols-3 auto-rows-min'>
             {testconfig.map(e =>
-                <Link className="text-white font-bold font-mono" key={e.path} to={e.path}>{e.path}</Link>
+                <Link className="text-white font-bold font-mono" key={e.path} to={e.path || '/'}>{e.path}</Link>
             )}
         </div>
     )
